e38bcf7a75e100249a2b5938ce409e108b152b60
artist0123 <napatart1111@gmail.com>
2023-03-28 21:03:50 +0700

    change ip from localhost to AWS

--- healthchecks/redis.sh
+++ healthchecks/redis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eo pipefail
 
-host="$(hostname -i || echo '127.0.0.1')"
+host="$(hostname -i || echo '54.145.199.161')"
 
 if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
 	exit 0
